2f1a45826e5229a8bfc7ecdfa284a4864b892d70
rutikeyone <rutikeyone@gmail.com>
2021-12-06 03:03:49 +0300

    Implementation Use cases

---
+++ app/src/main/java/com/mobiledev/cryptocurrencyapp/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,27 @@
+package com.mobiledev.cryptocurrencyapp.domain.use_case.get_coins
+
+import com.mobiledev.cryptocurrencyapp.common.Resource
+import com.mobiledev.cryptocurrencyapp.data.remote.dto.toCoin
+import com.mobiledev.cryptocurrencyapp.domain.model.Coin
+import com.mobiledev.cryptocurrencyapp.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(private val repository: CoinRepository) {
+
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins().map{it.toCoin()}
+            emit(Resource.Success(coins))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?:  "An unexpected error occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error(e.localizedMessage ?: "Couldn't reach server. Please check your internet connection"))
+        }
+    }
+
+}
